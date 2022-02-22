42594d7281b7a8d9f15f54cad2c44a2f036e6e95
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2022-02-22 10:50:32 +0100

    ivanpier

--- docs/js/script.js
+++ docs/js/script.js
@@ -53,6 +53,7 @@ async function login() {
     hds.append('Content-type', 'application/x-www-form-urlencoded');
     // hds.append('Authorization', 'basicAuth');
     // hds.append("Accept", "*/*");
+    /*
     hds.append('Access-Control-Allow-Origin', '*');
     hds.append('Cross-Origin-Resource-Policy', 'cross-origin');
     hds.append("Access-Control-Allow-Methods", "GET, POST, PUT");
@@ -60,6 +61,12 @@ async function login() {
     hds.append("Cache-Control", "no-cache");
     hds.append('Accept', 'application/json');
     hds.append('Authorization', 'authorizationHeader');
+    */
+    hds.append('Access-Control-Allow-Origin', '*'); // IMPORTANTE CORS “Access-Control-Allow-Origin” mancante
+    hds.append('Access-Control-Expose-Headers', 'Content-Length, X-JSON');
+    hds.append('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    hds.append('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, Accept-Language, X-Authorization');
+    hds.append('Access-Control-Max-Age: 86400');
     // let req = new Request(`${url}${params}`, { // for method get
     let req = new Request(url, {
         body: formData, // for post

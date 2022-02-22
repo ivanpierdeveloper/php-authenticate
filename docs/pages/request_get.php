01e70df63c391baac105409457572075a9b92329
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2022-02-22 09:38:38 +0100

    ivanpier

--- docs/pages/request_get.php
+++ docs/pages/request_get.php
@@ -1,5 +1,5 @@
 <?php
-    header("Content-type: application/json; charsdet=UTF-8");
+    header("Content-type: application/json; charset=UTF-8");
     echo '[
         {
             "username" : "Pluto",

2ca70786193f4e30b40dc342a8d49518f66a4622
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2022-02-22 12:42:46 +0100

    ivanpier

--- docs/pages/auth.php
+++ docs/pages/auth.php
@@ -1,7 +1,9 @@
 <?php
-    header('Access-Control-Allow-Origin: *'); // IMPORTANTE CORS “Access-Control-Allow-Origin” mancante
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: *");
+    // header('Access-Control-Allow-Origin: *'); // IMPORTANTE CORS “Access-Control-Allow-Origin” mancante
     header("Access-Control-Expose-Headers: Content-Length, X-JSON");
-    header("Access-Control-Allow-Methods: GET,HEAD,POST,DEBUG,PUT,DELETE,PATCH,OPTIONS");
+    // header("Access-Control-Allow-Methods: GET,HEAD,POST,DEBUG,PUT,DELETE,PATCH,OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Accept-Language, X-Authorization");
     header('Access-Control-Max-Age: 86400');
     

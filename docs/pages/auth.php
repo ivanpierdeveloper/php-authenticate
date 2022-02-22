14cb1c4295af8a5f7365130719c8c74315a9b7ac
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2022-02-22 10:07:37 +0100

    ivanpier

--- docs/pages/auth.php
+++ docs/pages/auth.php
@@ -1,8 +1,8 @@
 <?php
     header('Access-Control-Allow-Origin: *'); // IMPORTANTE CORS “Access-Control-Allow-Origin” mancante
-    header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
-    header ("Access-Control-Allow-Methods: GET,HEAD,POST,DEBUG,PUT,DELETE,PATCH,OPTIONS");
-    header ("Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Accept-Language, X-Authorization");
+    header("Access-Control-Expose-Headers: Content-Length, X-JSON");
+    header("Access-Control-Allow-Methods: GET,HEAD,POST,DEBUG,PUT,DELETE,PATCH,OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Accept-Language, X-Authorization");
     header('Access-Control-Max-Age: 86400');
     
     header('Content-Type: application/json; charset=UTF-8');

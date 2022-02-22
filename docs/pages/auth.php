aaf4745e7c9c1c0887ad319dc653d619e1feadca
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2022-02-22 12:54:35 +0100

    ivanpier

--- docs/pages/auth.php
+++ docs/pages/auth.php
@@ -2,12 +2,12 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: *");
     // header('Access-Control-Allow-Origin: *'); // IMPORTANTE CORS “Access-Control-Allow-Origin” mancante
-    header("Access-Control-Expose-Headers: Content-Length, X-JSON");
+    // header("Access-Control-Expose-Headers: Content-Length, X-JSON");
     // header("Access-Control-Allow-Methods: GET,HEAD,POST,DEBUG,PUT,DELETE,PATCH,OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Accept-Language, X-Authorization");
-    header('Access-Control-Max-Age: 86400');
-    
+    // header("Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Accept-Language, X-Authorization");
+    // header('Access-Control-Max-Age: 86400');
     header('Content-Type: application/json; charset=UTF-8');
+
     $valid_data    = array("pluto" => "abc123", "topolino" => "def456");
     $valid_user    = array_keys($valid_data);
     $valid_passwd  = array_values($valid_data);
